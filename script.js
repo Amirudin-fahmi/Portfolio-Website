37453e828734b19cbd8e6947e80e05fe7eee51c2
Amirudin-fahmi <amirudinfahmi27@gmail.com>
2024-04-30 13:41:44 +0700

    Personal Website

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings:["Web Developer","Web Designer","FrontEnd Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:500,
+    loop:true,
+
+});
